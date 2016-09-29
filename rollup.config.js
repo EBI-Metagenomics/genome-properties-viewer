84c29ff64b6fa7f793932a332c24c40ffccb6099
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2016-09-29 17:21:22 +0100

    including babel only if is creating a map

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
 import npm from "rollup-plugin-node-resolve";
 import babel from 'rollup-plugin-babel';
 
+var plugins =[npm({jsnext: true})];
+process.argv.forEach(function (val, index, array) {
+    if (val.startsWith("-m="))
+        plugins.push(babel());
+});
 export default {
     entry: "index.js",
-    plugins: [npm({jsnext: true}), babel()],
+    plugins: plugins,
     moduleId: "gpv",
     moduleName: "gpv",
     format: "umd"
