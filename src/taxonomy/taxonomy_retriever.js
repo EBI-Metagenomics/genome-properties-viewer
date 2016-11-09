35ad07f89b1112c215906846f9e8f48913fd5ee2
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2016-11-09 16:12:14 +0000

    using the pfam table taxonomy instead of ncbi_taxonomy

--- src/taxonomy/taxonomy_retriever.js
+++ src/taxonomy/taxonomy_retriever.js
@@ -19,17 +19,16 @@ var ids = [
     "667014", "768066", "945713", "1162668", "1360", "190650", "224308", "242619", "243275", "266117", "272843",
     "29343", "324602", "379066", "457570", "509192", "525909", "598659", "682795", "83332", "9606", "1163617",
     "1410620", "192222", "224324", "243164", "243277", "267608", "272944", "309799", "339670", "380703", "4577",
-    "511051", "546262", "59919", "690850", "83333", "999552"
+    "511051", "546262", "59919", "690850", "999552", "83333"
 ];
-// var ids = ["10090", "83333", "1033802", "1140", "1005058"];
+// var ids = ["999552","83333"];
+// var ids = ["243230", "246195", "272947"];
 var not_included = ids.slice();
 
 var taxon = {};
 
-function newNode(lineage, species, taxonomy, taxId, parent, children){
+function newNode(lineage, species, taxonomy, taxId, rank){
     taxId = taxId || null;
-    parent = parent || null;
-    children = children || [];
     var id = taxId || lineage+";"+species;
     return {
         id: id,
@@ -37,75 +36,80 @@ function newNode(lineage, species, taxonomy, taxId, parent, children){
         taxId: taxId,
         species: species,
         taxonomy: taxonomy,
-        parent: parent,
-        children: children
+        rank: rank,
+        parent: null,
+        children: []
     };
 }
 
 function newNodeFromRow(row){
+    var taxonomy = row.taxonomy.split(";").filter(d=>d.trim()!=""),
+        species = row.level;
     return newNode(
         row.taxonomy,
-        row.species,
-        row.taxonomy.split(";").filter(d=>d.trim()!=""),
+        species,
+        taxonomy,
         row.ncbi_taxid
     );
 }
 
-function buildAncestry(node){
-    if (node.parent==null){
-        if (node.taxonomy.length>0){
-            var tax = node.taxonomy.slice(0,node.taxonomy.length-1);
-            node.parent = tax.join(";");
-            if (node.parent!="") {
-                if (!(node.parent in taxon))
-                    taxon[node.parent] = newNode(node.parent, node.taxonomy[node.taxonomy.length - 1], tax);
-                buildAncestry(taxon[node.parent]);
+function buildAncestryFromDB(node, connection, left, right) {
+    var query ='SELECT * FROM pfam_30_0.taxonomy WHERE lft<'+left+' AND rgt>'+right+' AND minimal=1 ORDER BY lft DESC;';
+    var current = node;
+    connection.query(query, function(err, rows, fields) {
+        if (err) throw err;
+        for (var i=0;i<rows.length;i++) {
+            var row = rows[i];
+            if (!taxon[row.ncbi_taxid]){
+                taxon[row.ncbi_taxid] = newNodeFromRow(row);
+                taxon[row.ncbi_taxid].children.push(current);
+                current.parent = row.ncbi_taxid;
+                current = taxon[row.ncbi_taxid];
+            } else {
+                taxon[row.ncbi_taxid].children.push(current);
+                current.parent = row.ncbi_taxid;
+                current = taxon[row.ncbi_taxid];
+                break;
             }
-        } else {
-            return;
         }
-    }
-    if (node.parent!="" && taxon[node.parent].children.indexOf(node) == -1) {
-        taxon[node.parent].children.push(node);
-    }
-}
-
-function getRoots(){
-    var roots = Object.keys(taxon).map(d=>taxon[d]).filter(e=>e.parent=="");
-    if (roots.length==0) throw Error("UNVALID TREE: No roots found");
-    return roots;
-}
-function getRoot(){
-    var roots = getRoots();
-    return newNode("ROOT","ROOT",null,null,null,roots);
+        if (!current.parent && current.id !== 'root'){
+            taxon["root"].children.push(current);
+            current.parent = "root";
+        }
+    });
 }
 function annotate_number_of_leaves(node){
     if (node.number_of_leaves)
         return node.number_of_leaves;
     if (node.children.length<1)
-        node.number_of_leaves = 1
+        node.number_of_leaves = 1;
     else{
         node.number_of_leaves = 0;
         node.children.forEach(d=>node.number_of_leaves += annotate_number_of_leaves(d));
     }
     return node.number_of_leaves;
 }
+
 var connection = mysql.createConnection(jsonContent);
 connection.connect();
-var query ='SELECT * FROM pfam_30_0.ncbi_taxonomy WHERE ncbi_taxid IN ('+ids.join(",")+');';
+taxon["root"] = newNode("", "root", "", "root", null, []);
+var query ='SELECT * FROM pfam_30_0.taxonomy WHERE ncbi_taxid IN ('+ids.join(",")+');';
 connection.query(query, function(err, rows, fields) {
     if (err) throw err;
-    rows.forEach(row=>{
-        taxon[row.taxonomy] = newNodeFromRow(row);
-        taxon[row.taxonomy].number_of_leaves = 1;
-        buildAncestry(taxon[row.taxonomy]);
+    rows.forEach(row => {
+        if (!row.ncbi_taxid) return;
+        taxon[row.ncbi_taxid] = newNodeFromRow(row);
+        taxon[row.ncbi_taxid].number_of_leaves = 1;
+        buildAncestryFromDB(taxon[row.ncbi_taxid], connection, row.lft, row.rgt);
         var index =  not_included.indexOf(String(row.ncbi_taxid));
         if (index!=-1)
             not_included.splice(index, 1);
     });
-    connection.end();
-    var root = getRoot();
-    annotate_number_of_leaves(root);
-    console.log(JSON.stringify(root));
+    connection.end(function(err){
+        // var root = getRoot();
+        annotate_number_of_leaves(taxon["root"]);
+        console.log(JSON.stringify(taxon["root"]));
+
+    });
 });
 
