ecc1ae12e54502579b9e00f588dd94b577b95904
Gustavo A. Salazar <gsalazar@ebi.ac.uk>
2018-12-13 16:46:20 +0000

    upgrading d3 libs and redifining category20b

--- src/d3.js
+++ src/d3.js
@@ -14,12 +14,13 @@ export {
 export { transition } from "d3-transition";
 export { stratify, cluster, tree, hierarchy } from "d3-hierarchy";
 export { entries, keys, values } from "d3-collection";
-export {
-  scaleLinear,
-  scaleBand,
-  scaleOrdinal,
-  schemeCategory20b
-} from "d3-scale";
+export { scaleLinear, scaleBand, scaleOrdinal } from "d3-scale";
+
+const colors = s => s.match(/.{6}/g).map(x => "#" + x);
+export const schemeCategory20b = colors(
+  "393b795254a36b6ecf9c9ede6379398ca252b5cf6bcedb9c8c6d31bd9e39e7ba52e7cb94843c39ad494ad6616be7969c7b4173a55194ce6dbdde9ed6"
+);
+
 export { tsvParseRows } from "d3-dsv";
 export { zoom } from "d3-zoom";
 export { interpolate } from "d3-interpolate";
